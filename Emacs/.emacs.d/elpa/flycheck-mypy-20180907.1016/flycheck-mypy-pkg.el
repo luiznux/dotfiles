ca4242fb2d8f17e541020a054f47e0927c7dd218
luiz tagliaferro <luiztagli@hotmail.com>
2019-07-02 19:56:39 -0300

    remove arquivos desnecessarios

--- Emacs/.emacs.d/elpa/flycheck-mypy-20180907.1016/flycheck-mypy-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "flycheck-mypy" "20180907.1016" "Support mypy in flycheck" '((flycheck "0.18")) :commit "6f99166f5229c7b4298cff1818b7eaece1c9c8bd" :authors '(("Lorenzo Bolla" . "lbolla@gmail.com")) :maintainer '("Lorenzo Bolla" . "lbolla@gmail.com"))
--- Emacs/.emacs.d/elpa/nov-20190115.1854/nov-autoloads.el
+++
@@ -1,21 +0,0 @@
-;;; nov-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "nov" "nov.el" (23801 42378 560609 602000))
-;;; Generated autoloads from nov.el
-
-(autoload 'nov-mode "nov" "\
-Major mode for reading EPUB documents
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; nov-autoloads.el ends here
