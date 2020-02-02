f54cafbc58159f4be3009e22a4d3ea971ef72e08
luiznux <luiztagli@hotmail.com>
2020-02-02 07:33:25 -0300

    add emacs packages

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -43,11 +43,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#1d1f21" :foreground "#c5c8c6" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 148 :width normal :foundry "ADBO" :family "Source Code Pro")))))
+ '(default ((t (:inherit nil :stipple nil :background "#1d1f21" :foreground "#c5c8c6" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 129 :width normal :foundry "ADBO" :family "Source Code Pro")))))
 
 (global-hl-line-mode 1)
 
-(load "~/.emacs.d/main.el")
+;(global-diff-hl-mode)                                                                  
+;(global-nlinum-mode)   
 
+(load "~/.emacs.d/main.el")
 
 ;;; init.el ends here
