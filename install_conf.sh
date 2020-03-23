d59958fbf20783b329f5f8152d6d78ce7a256051
luiznux <luiztagli@hotmail.com>
2020-03-23 17:43:01 -0300

    update file

--- install_conf.sh
+++ install_conf.sh
@@ -18,19 +18,19 @@ cd ~/Github && git init
 echo "                        Directory tree {OK}"
 
 echo "#----------------------------------------------- Packages"
-echo "		Installing packages"
+echo "     Installing packages"
 sudo pacman -Sy xorg man gvim tree rxvt-unicode rxvt-unicode-terminfo urxvt-perls cmake libmpdclient wget i3-gaps i3lock-color ranger w3m nemo nemo-fileroller papirus-icon-theme sl feh vlc htop gnome-calculator noto-fonts-cjk noto-fonts-emoji noto-fonts clang tlp i7z cpupower alsa calcurse pulseaudio ttf-font-awesome libxss libcurl-gnutls dmenu mailutils llvm dhcp dhcpcd haveged xreader calibre ristretto tumbler evince playerctl check gobject-introspection transmission-gtk file ffmpegthumbnailer highlight atool imagemagick fftw openjdk11-src lxrandr-gtk3 mtpfs gvfs-mtp gvfs-gphoto2 android-file-transfer libmtp ufw sxiv yasm lxappearance gtk-chtheme xorg-xinit intltool
-echo " 	                            Packages {OK}"
+echo "     Packages {OK}"
 
 echo "#----------------------------------------------- PYTHON CONFIG"
-echo "		Python config"
+echo "     Python config"
 sudo pacman -S python python2 python-pip python-sphinx python-dbus python2-gobject pygtk python-psutil python-urwid --noconfirm
-echo "		                        Python {OK}"
+echo "	   Python {OK}"
 
 echo "#----------------------------------------------- Graphic drives and NVIDIA"
-echo "		Graphic drivers"
+echo "	   Graphic drivers"
 sudo pacman -S xf86-video-intel vulkan-intel mesa-demos nvidia nvidia-utils nvidia-settings bumblebee --noconfirm
-echo "		Graphic Drivers {OK}"
+echo "	   Graphic Drivers {OK}"
 
 echo "#---------------------------------------- AUR packages"
 echo "Installing some AUR Packages"
@@ -71,10 +71,10 @@ sudo pacman -S acpi libinput xf86-input-synaptics xorg-xinput powertop xfce4-pow
 
 echo "#----------------------------------------EMACS INSTALL"
 cp -r emacs/.emacs.d  ~/.emacs.d/
-echo "          Emacs config {OK} "
+echo "     Emacs config {OK} "
 cd ~/ && wget gnu.c3sl.ufpr.br/ftp/emacs/emacs-26.3.tar.xz && tar -xvf emacs-26.3.tar.xz && rm emacs-26.3.tar.xz
 cd ~/emacs-26.3 && ./autogen.sh && ./configure && make && sudo make install
-echo "        Emacs  Install  {OK}"
+echo "     Emacs  Install  {OK}"
 
 echo "#----------------------------------------Setup Pacman config"
 sudo cp config/pacman/mirrorlist /etc/pacman.d/
@@ -83,12 +83,12 @@ sudo rm /etc/pacman.conf || sudo cp config/pacman/pacman.conf  /etc/
 echo "#----------------------------------------Setup i3 and polybar"
 cp config/i3/config ~/.config/i3/
 cp -R config/polybar/  ~/.config/polybar/
-echo "        I3 and Polybar config {OK} "
+echo "     I3 and Polybar config {OK} "
 
 echo "#----------------------------------------Polyabar Scripts"
 cd ~/.config/polybar/modules/ && git clone https://github.com/kamek-pf/polybar-forecast.git
 cd ~/.config/polybar/modules/polybar-forecast/ && cargo build --release
-echo"				   	Scripts    {OK}"
+echo"	   Scripts    {OK}"
 
 echo "#----------------------------------------Ranger config"
 mkdir ~/.config/ranger
@@ -99,7 +99,7 @@ echo "#----------------------------------------Vim config setup"
 mkdir ~/.vim
 cp vim/.vimrc ~/.vimrc
 cp -r vim/.vim/ ~/
-echo "             Vim setup {OK} "
+echo "     Vim setup {OK} "
 
 echo "#----------------------------------------Light(brithness control)"
 cd ~/Github/prog/ && git clone https://github.com/haikarainen/light
@@ -107,7 +107,7 @@ cd ~/Github/prog/light && ./autogen.sh && ./configure && sudo make
 
 echo "#----------------------------------------Setup font"
 sudo cp -R config/fonts/source-code-pro /usr/local/share/fonts/
-echo "           Fount setup {OK} "
+echo "     Fount setup {OK} "
 
 echo "#----------------------------------------Setup Xresources"
 cp config/.Xresources ~/.Xresources  && xrdb -laod ~/.Xresources
@@ -117,11 +117,11 @@ echo "#----------------------------------------Setup gitignore global file"
 cp config/.gitignore_global  ~/
 echo "       Gitignore global setup {OK} "
 cp config/.gitconfig ~/
-echo "              Gitconfig setup {OK} "
+echo "       Gitconfig setup {OK} "
 
 echo"#----------------------------------------Setup background image"
 cp config/blue-hair-girl.jpg ~/.config/wallpaper.jpg
-echo "              Wallppaer setup {OK} "
+echo "       Wallppaer setup {OK} "
 
 echo"#----------------------------------------Setup Themes"
 cd gtk-themes/gtk && tar -xvf Midnight-BlueNight-Theme.tar.xz && sudo mv Midnight-BlueNight-Theme/  /usr/share/themes/
