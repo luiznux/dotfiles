0bd559ddfa9c74787f55db61e56a67710dfa4832
luiznux <luiztagli@hotmail.com>
2020-05-08 18:53:50 -0300

    update temperature string

--- polybar/scripts/system-cpu-temppercore.sh
+++ polybar/scripts/system-cpu-temppercore.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 #notebook format
-temp=$(sensors | grep "CPU" | cut -b 18,19,20,21,22,23,24)
+temp=$(sensors | grep "CPU" | cut -b 18,19,22,23,24)
 
 #desktop format
-#temp=$(sensors | grep "Package id 0" | cut -b 17,18,19,20,21,22,23)
+#temp=$(sensors | grep "Package id 0" | cut -b 17,18,21,22,23)
 
 echo $temp
