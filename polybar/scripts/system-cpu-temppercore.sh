1d0804c1a9a818899568effebf463919500e310e
luiznux <luiztagli@hotmail.com>
2020-05-08 18:45:39 -0300

    add desktop config

--- polybar/scripts/system-cpu-temppercore.sh
+++ polybar/scripts/system-cpu-temppercore.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-temp=$(sensors | grep "CPU" | cut -d: -f2)
+#notebook format
+temp=$(sensors | grep "CPU" | cut -b 18,19,20,21,22,23,24)
+
+#desktop format
+#temp=$(sensors | grep "Package id 0" | cut -b 17,18,19,20,21,22,23)
 
 echo $temp
