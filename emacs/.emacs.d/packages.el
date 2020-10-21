f2e6cba50c602d123de56e25c94eb181751bf1b6
luiznux <luiztagli@hotmail.com>
2020-10-21 02:24:52 -0300

    remove duplicate(treemacs) and add new dashboard-config(melpa)

--- emacs/.emacs.d/packages.el
+++ emacs/.emacs.d/packages.el
@@ -146,116 +146,41 @@
 ;  (set-face-foreground 'linum "#8b8bcd")
 ;  (set-face-foreground 'highlight nil))
 
-(use-package treemacs
-  :ensure t
-  :defer t
-  :config
-  (progn
-    (setq treemacs-collapse-dirs                 (if treemacs-python-executable 3 0)
-          treemacs-deferred-git-apply-delay      0.5
-          treemacs-directory-name-transformer    #'identity
-          treemacs-display-in-side-window        t
-          treemacs-eldoc-display                 t
-          treemacs-file-event-delay              5000
-          treemacs-file-extension-regex          treemacs-last-period-regex-value
-          treemacs-follow-mode                   t
-          treemacs-file-follow-delay             0.1
-          treemacs-filewatch-mode                nil
-          treemacs-file-name-transformer         #'identity
-          treemacs-follow-after-init             t
-          treemacs-git-command-pipe              ""
-          treemacs-goto-tag-strategy             'refetch-index
-          treemacs-indentation                   1
-          treemacs-indentation-string            " "
-          treemacs-is-never-other-window         nil
-          treemacs-max-git-entries               5000
-          treemacs-missing-project-action        'ask
-          treemacs-no-png-images                 nil
-          treemacs-fringe-indicator-mode         t
-          treemacs-no-delete-other-windows       t
-          treemacs-project-follow-cleanup        1
-          treemacs-persist-file                  (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
-          treemacs-position                      'left
-          treemacs-recenter-distance             0.1
-          treemacs-recenter-after-file-follow    1
-          treemacs-recenter-after-tag-follow     1
-          treemacs-recenter-after-project-jump   'always
-          treemacs-recenter-after-project-expand 'on-distance
-          treemacs-show-cursor                   t
-          treemacs-show-hidden-files             t
-          treemacs-silent-filewatch              nil
-          treemacs-silent-refresh                nil
-          treemacs-sorting                       'alphabetic-asc
-          treemacs-space-between-root-nodes      t
-          treemacs-tag-follow-cleanup            t
-          treemacs-tag-follow-delay              0.1
-          treemacs-user-mode-line-format         nil
-          treemacs-width                         15)
-    ;; The default width and height of the icons is 22 pixels. If you are
-    ;; using a Hi-DPI display, uncomment this to double the icon size.
-    ;;(treemacs-resize-icons 44)
-    (treemacs-get-icon-value 'root nil "Default")
-    (treemacs-get-icon-value "org" t)
-    (pcase (cons (not (null (executable-find "git")))
-                 (not (null treemacs-python-executable)))
-      (`(t . t)
-       (treemacs-git-mode 'deferred))
-      (`(t . _)
-       (treemacs-git-mode 'simple))))
-  :bind
-  (:map global-map
-        ("M-0"       . treemacs-select-window)
-        ("C-x t 1"   . treemacs-delete-other-windows)
-        ("C-x t t"   . treemacs)
-        ("C-x t B"   . treemacs-bookmark)
-        ("C-x t C-t" . treemacs-find-file)
-        ("C-x t M-t" . treemacs-find-tag)))
-
-(use-package treemacs-evil
-  :after treemacs evil
-  :ensure t)
-
-(use-package treemacs-projectile
-  :after treemacs projectile
-  :ensure t)
-
-(use-package treemacs-icons-dired
-  :after treemacs dired
-  :ensure t
-  :config (treemacs-icons-dired-mode))
-
-(use-package treemacs-magit
-  :after treemacs magit
-  :ensure t)
-
-(use-package treemacs-persp
-  :after treemacs persp-mode
-  :ensure t
-  :config (treemacs-set-scope-type 'Perspectives))
-
 (use-package all-the-icons
   :ensure t)
 
 (use-package rainbow-mode
   :ensure t)
 
-(use-package evil-leader
-  :ensure t)
-
-(use-package evil-org
-  :ensure t)
-
 (use-package evil-mode
   :hook (org-mode . evil-org-mode))
-
-(use-package undo-tree ;dependency for evil-undo-system
-  :ensure t
-  :config
-  (global-undo-tree-mode))
 
 (use-package org-super-agenda
   :ensure t
   :config (org-super-agenda-mode t))
+
+(use-package dashboard
+  :ensure t
+  :init
+  (progn
+    (setq dashboard-items '((recents  . 7)
+                            (projects . 6))))
+  :config
+  (dashboard-setup-startup-hook)
+  (add-hook 'dashboard-mode-hook (lambda () (org-agenda t "x")) (lambda () (ace-window)))
+
+  (setq dashboard-set-heading-icons  t
+        dashboard-set-file-icons     t
+        dashboard-set-navigator      t
+        dashboard-startup-banner     'logo)
+
+  (setq dashboard-navigator-buttons
+        `(;;line1
+          ((,(all-the-icons-octicon "mark-github" :height 1.1 :v-adjust 0.0)
+            "Homepage"
+            "Browse homepage"
+            (lambda (&rest _) (browse-url "https://github.com/luiznux")))
+           (" " "Refresh" "Refresh" (lambda (&rest _) (dashboard-refresh-buffer)) nil)))))
 
 (use-package minimap
   :ensure t
@@ -301,7 +226,6 @@
 ;Local packages(github)
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp/awesome-tab"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp/emacs-dashboard"))
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp/page-break-lines"))
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp/origami.el"))
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp/nerd-fonts.el"))
@@ -310,28 +234,6 @@
   (require 'awesome-tab)
   (setq awesome-tab-display-icon t)
   (awesome-tab-mode t))
-
-(defun setup-emacs-dashboard()
-  (require 'dashboard)
-  (dashboard-setup-startup-hook)
-  (add-hook 'dashboard-mode-hook (lambda () (org-agenda t "x")) (lambda () (ace-window)))
-
-  (setq dashboard-itens '((bookmarks .  5)))
-
-  (setq dashboard-set-heading-icons  t
-        dashboard-set-file-icons     t
-        show-week-agenda-p           t
-        dashboard-set-navigator      t
-        dashboard-startup-banner     'logo)
-
- ;; Format: "(icon title help action face prefix suffix)"
-(setq dashboard-navigator-buttons
-      `(;;line1
-        ((,(all-the-icons-octicon "mark-github" :height 1.1 :v-adjust 0.0)
-           "Homepage"
-           "Browse homepage"
-           (lambda (&rest _) (browse-url "https://github.com/luiznux")))
-         (" " "Refresh" "Refresh" (lambda (&rest _) (dashboard-refresh-buffer)) nil)))))
 
 (defun setup-page-break-lines()
   (require 'page-break-lines)
@@ -360,7 +262,6 @@
 ;(setup-irony-packages)
 (setup-git-packages)
 (setup-awesome-tab)
-(setup-emacs-dashboard)
 (setup-page-break-lines)
 (setup-origami-mode)
 ;(setup-nerd-fonts-el)
