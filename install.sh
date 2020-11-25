f914cf1f77b86460fba723f524b1791760afe40f
luiznux <luiztagli@hotmail.com>
2020-11-25 00:22:41 -0300

    remove spicetify package

--- install.sh
+++ install.sh
@@ -129,7 +129,6 @@ AUR_install(){
     && log_error make_pkg_AUR python2-twodict-git \
     && log_error make_pkg_AUR youtube-dl-gui-git \
     && log_error make_pkg_AUR jetbrains-toolbox \
-    && log_error make_pkg_AUR spicetify-themes-git \
     && log_error make_pkg_AUR ttf-wps-fonts \
     && log_error make_pkg_AUR wps-office \
     && log_error make_pkg_AUR wps-office-mui \
