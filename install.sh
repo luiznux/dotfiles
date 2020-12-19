5ac787153f8ee35df97ea85fb93da3b8986b83de
luiznux <luiztagli@hotmail.com>
2020-12-19 00:46:07 -0300

    add new home folders
    -> tests(store my tests)
    -> projects/{personal,work} add separade folders for project type

--- install.sh
+++ install.sh
@@ -89,7 +89,7 @@ clean_log(){
 dir_tree(){
 
     log echo "#----------------------------------------------- Setup directory tree"
-    mkdir -vp ~/{Github/{luiznux,prog,other},AUR,Torrents,Mangas,Books,Isos,Calibre-Library,Videos,Music,Downloads,Pictures/Screenshots,Documents,Desktop,projects,.vim,.config/{i3,polybar,ranger}} \
+    mkdir -vp ~/{Github/{luiznux,prog,other},AUR,Torrents,Mangas,Books,Isos,Calibre-Library,Videos,Music,Downloads,Pictures/Screenshots,Documents,Desktop,tests,projects/{personal,work},.vim,.config/{i3,polybar,ranger}} \
     && log echo "        Directory tree {OK}" && break_line || log erro_msg
 }
 
