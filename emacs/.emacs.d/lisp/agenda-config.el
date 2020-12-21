274ce2c49bbe7222daa9ac4bfe574c6ebf9d3919
luiznux <luiztagli@hotmail.com>
2020-12-21 14:16:16 -0300

    add "WORKING" to org todo keywords

--- emacs/.emacs.d/lisp/agenda-config.el
+++ emacs/.emacs.d/lisp/agenda-config.el
@@ -84,12 +84,13 @@
 (defun org-TODO-keywords ()
   "Add and customize org TODO keywords."
   (setq org-todo-keywords
-        '((sequence "TODO" "NOTE" "IN-PROGRESS" "WAITING" "IMPORTANT" "DONE")))
+        '((sequence "TODO" "NOTE" "IN-PROGRESS" "WAITING" "WORKING" "IMPORTANT" "DONE")))
 
   (setq org-todo-keyword-faces '(("TODO"         . (:foreground "#ff8080" :weight bold))
                                  ("NOTE"         . (:foreground "#ffe9aa" :weight bold))
                                  ("IN-PROGRESS"  . (:foreground "#A020F0" :weight bold))
                                  ("WAITING"      . (:foreground "#ffb378" :weight bold))
+                                 ("WORKING"      . (:foreground "#4db5bd" :weight bold))
                                  ("IMPORTANT"    . (:foreground "#f32020" :weight bold))
                                  ("DONE"         . (:foreground "#1E90FF" :weight bold)))))
 
