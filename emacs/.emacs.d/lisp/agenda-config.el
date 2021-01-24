d4d879399ea0d10c16d3a33a7f46b19b634d48e5
luiznux <luiztagli@hotmail.com>
2021-01-24 03:46:10 -0300

    migrate packages and configs to org-config.el

--- emacs/.emacs.d/lisp/agenda-config.el
+++ emacs/.emacs.d/lisp/agenda-config.el
@@ -1,6 +1,6 @@
-;;; package --- Summary
+;;; agenda-config.el --- Package for config org agenda
 ;;; Commentary:
-;;;
+;;; Emacs org agenda config
 ;;; Code:
 
 (defun agenda-vim-mode ()
@@ -11,7 +11,6 @@
        (evil-define-key 'normal org-agenda-mode-map
          (kbd "<RET>") 'org-agenda-switch-to
          (kbd "\t") 'org-agenda-goto
-
          "q" 'org-agenda-quit
          "r" 'org-agenda-redo
          "S" 'org-save-all-org-buffers
@@ -71,30 +70,6 @@
          "[" 'org-agenda-manipulate-query-add
          "g\\" 'org-agenda-filter-by-tag-refine
          "]" 'org-agenda-manipulate-query-subtract))))
-
-
-(defun org-capture-config ()
-  "Org caputer config."
-  (setq org-default-notes-file "~/org/capture.org")
-  (global-set-key (kbd "C-c c") 'org-capture))
-
-(defun org-tags-set ()
-  "Set org agenda custom tags."
- (setq org-tag-alist '(("work " . ?w) ("coll " . ?c) ("personal" . ?p))))
-
-(defun org-TODO-keywords ()
-  "Add and customize org TODO keywords."
-  (setq org-todo-keywords
-        '((sequence "TODO" "NOTE" "IMPORTANT" "WAITING" "CANCELLED" "IN-PROGRESS"  "WORKING"  "DONE")))
-
-  (setq org-todo-keyword-faces '(("TODO"         . (:foreground "#ff8080" :weight bold))
-                                 ("NOTE"         . (:foreground "#ffe9aa" :weight bold))
-                                 ("IMPORTANT"    . (:foreground "#f32020" :weight bold))
-                                 ("WAITING"      . (:foreground "#ffb378" :weight bold))
-                                 ("CANCELLED"    . (:foreground "#ff6c6b" :weight bold))
-                                 ("IN-PROGRESS"  . (:foreground "#A020F0" :weight bold))
-                                 ("WORKING"      . (:foreground "#4db5bd" :weight bold))
-                                 ("DONE"         . (:foreground "#1E90FF" :weight bold)))))
 
 (defun org-agenda-custom-config ()
   (setq org-agenda-custom-commands
@@ -156,11 +131,7 @@ from the left."
   )
 ;;-------------------
 
-
 (agenda-vim-mode)
-(org-capture-config)
-(org-tags-set)
-(org-TODO-keywords)
 (org-agenda-custom-config)
 (add-hook 'org-agenda-finalize-hook #'ll/org/colorize-headings)
 
