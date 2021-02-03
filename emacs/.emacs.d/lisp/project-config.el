2fe16c297fea62e638df373db4b392fa260be1cf
luiznux <luiztagli@hotmail.com>
2021-02-02 23:11:00 -0300

    add new treemacs configs

--- emacs/.emacs.d/lisp/project-config.el
+++ emacs/.emacs.d/lisp/project-config.el
@@ -48,10 +48,13 @@
     (progn
       (setq treemacs-collapse-dirs                 (if (treemacs--find-python3) 3 0)
             treemacs-deferred-git-apply-delay      0.5
+            treemacs-directory-name-transformer    #'identity
             treemacs-display-in-side-window        t
             treemacs-eldoc-display                 t
             treemacs-file-event-delay              5000
+            treemacs-file-extension-regex          treemacs-last-period-regex-value
             treemacs-file-follow-delay             0.2
+            treemacs-file-name-transformer         #'identity
             treemacs-follow-after-init             t
             treemacs-git-command-pipe              ""
             treemacs-goto-tag-strategy             'refetch-index
@@ -60,11 +63,13 @@
             treemacs-is-never-other-window         nil
             treemacs-max-git-entries               5000
             treemacs-missing-project-action        'ask
+            treemacs-move-forward-on-expand        nil
             treemacs-no-png-images                 nil
             treemacs-no-delete-other-windows       t
             treemacs-project-follow-cleanup        nil
             treemacs-persist-file                  (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
             treemacs-position                      'left
+            treemacs-read-string-input             'from-child-frame
             treemacs-recenter-distance             0.1
             treemacs-recenter-after-file-follow    nil
             treemacs-recenter-after-tag-follow     nil
@@ -78,12 +83,19 @@
             treemacs-space-between-root-nodes      t
             treemacs-tag-follow-cleanup            t
             treemacs-tag-follow-delay              1.5
-            treemacs-width                         36)
+            treemacs-user-mode-line-format         nil
+            treemacs-user-header-line-format       nil
+            treemacs-workspace-switch-cleanup      nil
+            treemacs-width                         35)
 
+      ;; The default width and height of the icons is 22 pixels. If you are
+      ;; using a Hi-DPI display, uncomment this to double the icon size.
+      ;;(treemacs-resize-icons 44)
+
+      ;(treemacs-load-theme "alltheicons")
       (treemacs-follow-mode t)
       (treemacs-filewatch-mode t)
       (treemacs-resize-icons 18)
-      (treemacs-load-theme "alltheicons")
       (treemacs-fringe-indicator-mode t)
       (pcase (cons (not (null (executable-find "git")))
                    (not (null (treemacs--find-python3))))
