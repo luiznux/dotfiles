34916b676a6c20da35022a1d65edc2a652afed06
luiznux <luiztagli@hotmail.com>
2021-03-09 15:24:28 -0300

    create new folder "scripts"

--- install.sh
+++ install.sh
@@ -82,7 +82,7 @@ clean_AUR(){
 dir_tree(){
 
     log echo "#----------------------------------------------- Setup directory tree"
-    mkdir -vp ~/{Github/{luiznux,prog,other},AUR,Torrents,Mangas,Books,Isos,Calibre-Library,Videos,Music,Downloads,Pictures/Screenshots,Documents,Desktop,tests,projects/{personal,work},.vim,.config/{i3,polybar,ranger,rofi,alacritty}} \
+    mkdir -vp ~/{Github/{luiznux,prog,other},AUR,Torrents,Mangas,Books,Isos,Calibre-Library,Videos,Music,Downloads,Pictures/Screenshots,Documents,Desktop,tests,projects/{personal,work},.vim,.config/{i3,polybar,ranger,rofi,alacritty,scripts}} \
     && log echo "        Directory tree {OK}" && break_line || log erro_msg
 }
 
