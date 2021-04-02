5e10fc95b5f2933258286927504036d5f71dee51
luiznux <luiztagli@hotmail.com>
2021-04-02 04:52:18 -0300

    add mic script and icon to polybar

---
+++ polybar/scripts/mic.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#variables
+pidfile="/var/run/user/$UID/polybar-mic-script.pid"
+
+#Colors
+COLOR_UNMUTED="%{F#00FF00}"
+COLOR_MUTED="%{F#de0000}"
+
+#icon format
+micON="${COLOR_UNMUTED}"
+micOFF="${COLOR_MUTED}"
+
+
+if [ -e "$pidfile" ]; then
+	kill "$(cat "${pidfile}")" > /dev/null 2>&1
+	cleanup
+fi
+
+cleanup() {
+	rm -f "$pidfile"
+}
+
+#Return(with echo) the status of the microphone
+print_status() {
+	if amixer get Capture | grep -q '\[off\]'; then
+		echo $micOFF${COLOR_MUTED}
+	else
+		echo $micON${COLOR_UNMUTED}
+	fi
+}
+
+#Toggle the microphone status then update it with the print_status func
+toggle() {
+	amixer set Capture toggle > /dev/null 2>&1
+	print_status
+}
+
+#Get the inputs
+trap "cleanup" EXIT
+trap "toggle" USR1
+
+echo $$ > "$pidfile"
+
+### The "main" func
+while true; do
+	print_status
+	sleep 1 &
+	wait
+done
