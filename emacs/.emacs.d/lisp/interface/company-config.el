430da839e87c4e0ed0e436246ebd261640084022
luiznux <luiztagli@hotmail.com>
2021-05-05 11:05:34 -0300

    add new company features

--- emacs/.emacs.d/lisp/interface/company-config.el
+++ emacs/.emacs.d/lisp/interface/company-config.el
@@ -1,6 +1,6 @@
 ;;; company-config.el --- Company groups and packages configs
 ;;; Commentary:
-;;; Configuration and customization on company package and dependecies
+;;; Configuration and customization on company package and dependences
 ;;;
 ;;; Code:
 
@@ -8,36 +8,100 @@
   "Configure all company packages and custom."
 
   (use-package company
+
+    :defines (company-dabbrev-ignore-case company-dabbrev-downcase)
+    :commands company-cancel
     :bind
     (:map company-active-map
           ("M-j" . company-select-next-or-abort)
           ("M-k" . company-select-previous-or-abort)
           ("<tab>" . company-complete-common-or-cycle))
     :hook ( after-init . global-company-mode)
-    :config
-    (setq company-minimum-prefix-length  1
+    :init
+    (setq company-tooltip-align-annotations t
+          company-minimum-prefix-length  1
           company-tooltip-minimum-width  60
           company-tooltip-minimum        25
           company-tooltip-limit          15
           company-idle-delay             0
-          company-echo-delay             0))
+          company-echo-delay             0
+          company-dabbrev-ignore-case    nil
+          company-dabbrev-downcase       nil
+          company-global-modes '(not erc-mode message-mode help-mode
+                                     gud-mode eshell-mode shell-mode)
+          company-backends '((company-capf :with company-yasnippet)
+                             (company-dabbrev-code company-keywords company-files)
+                             company-dabbrev)))
+
+  (defun my-company-yasnippet ()
+    "Hide the current completeions and show snippets."
+    (interactive)
+    (company-cancel)
+    (call-interactively 'company-yasnippet))
+
+  :config
+  ;; `yasnippet' integration
+  (with-no-warnings
+    (with-eval-after-load 'yasnippet
+      (defun company-backend-with-yas (backend)
+        "Add `yasnippet' to company backend."
+        (if (and (listp backend) (member 'company-yasnippet backend))
+            backend
+          (append (if (consp backend) backend (list backend))
+                  '(:with company-yasnippet))))
+
+      (defun my-company-enbale-yas (&rest _)
+        "Enable `yasnippet' in `company'."
+        (setq company-backends (mapcar #'company-backend-with-yas company-backends)))
+
+      (defun my-lsp-fix-company-capf ()
+        "Remove redundant `comapny-capf'."
+        (setq company-backends
+              (remove 'company-backends (remq 'company-capf company-backends))))
+      (advice-add #'lsp-completion--enable :after #'my-lsp-fix-company-capf)
+
+      (defun my-company-yasnippet-disable-inline (fun command &optional arg &rest _ignore)
+        "Enable yasnippet but disable it inline."
+        (if (eq command 'prefix)
+            (when-let ((prefix (funcall fun 'prefix)))
+              (unless (memq (char-before (- (point) (length prefix)))
+                            '(?. ?< ?> ?\( ?\) ?\[ ?{ ?} ?\" ?' ?`))
+                prefix))
+          (progn
+            (when (and (bound-and-true-p lsp-mode)
+                       arg (not (get-text-property 0 'yas-annotation-patch arg)))
+              (let* ((name (get-text-property 0 'yas-annotation arg))
+                     (snip (format "%s (Snippet)" name))
+                     (len (length arg)))
+                (put-text-property 0 len 'yas-annotation snip arg)
+                (put-text-property 0 len 'yas-annotation-patch t arg)))
+            (funcall fun command arg))))
+      (advice-add #'company-yasnippet :around #'my-company-yasnippet-disable-inline)))
+
+  ;; Better sorting and filtering
+  (use-package company-prescient
+    :init (company-prescient-mode 1))
 
   (use-package company-box
+    :defines company-box-icons-all-the-icons
     :hook (company-mode . company-box-mode)
     :init (setq company-box-icons-alist 'company-box-icons-all-the-icons)
     :config
     (setq company-box-backends-colors         nil
           company-box-show-single-candidate   t)
 
-    (defun company-box-icons--elisp (candidate)
-      (when (derived-mode-p 'emacs-lisp-mode)
-        (let ((sym (intern candidate)))
-          (cond ((fboundp sym) 'Function)
-                ((featurep sym) 'Module)
-                ((facep sym) 'Color)
-                ((boundp sym) 'Variable)
-                ((symbolp sym) 'Text)
-                (t . nil)))))
+    (with-no-warnings
+      ;; Prettify icons
+      (defun my-company-box-icons--elisp (candidate)
+        (when (or (derived-mode-p 'emacs-lisp-mode) (derived-mode-p 'lisp-mode))
+          (let ((sym (intern candidate)))
+            (cond ((fboundp sym) 'Function)
+                  ((featurep sym) 'Module)
+                  ((facep sym) 'Color)
+                  ((boundp sym) 'Variable)
+                  ((symbolp sym) 'Text)
+                  (t . nil)))))
+      (advice-add #'company-box-icons--elisp :override #'my-company-box-icons--elisp))
 
     (with-eval-after-load 'all-the-icons
       (declare-function all-the-icons-faicon 'all-the-icons)
@@ -80,19 +144,13 @@
     :bind (:map company-active-map
                 ("M-h" . company-quickhelp-manual-begin))
     :hook (global-company-mode . company-quickhelp-mode)
-    :custom (company-quickhelp-delay 0.8))
+    :custom (company-quickhelp-delay 0.5))
 
   (use-package company-posframe
     :hook (company-mode . company-posframe-mode)))
 
-;;  (use-package company-lsp
-;;    :config
-;;    (company-lsp-enable-snippet t)
-;;    (company-lsp-cache-candidates 'auto)
-;;    :commands company-lsp))
-
 
 (setup-company-config)
 
-(provide 'company-config.el)
+(provide 'company-config)
 ;;; company-config.el ends here
