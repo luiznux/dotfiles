3b15fbf778e929bf415c2eb248aafa7ac3350fe8
luiznux <luiztagli@hotmail.com>
2021-06-19 01:42:24 -0300

    remove whitespaces

--- emacs/.emacs.d/lisp/extra/customizations.el
+++ emacs/.emacs.d/lisp/extra/customizations.el
@@ -67,7 +67,7 @@
     (global-set-key [(hyper z)] 'undo)
 
     ;; mac switch meta key
-    (defun mac-switch-meta nil 
+    (defun mac-switch-meta nil
       "switch meta between Option and Command"
       (interactive)
       (if (eq mac-option-modifier nil)
@@ -75,7 +75,7 @@
 	        (setq mac-option-modifier 'meta)
 	        (setq mac-command-modifier 'hyper)
 	        )
-        (progn 
+        (progn
           (setq mac-option-modifier nil)
           (setq mac-command-modifier 'meta)))))))
 
