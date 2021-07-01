8e92c39b8493fc016c043d935afb1e2b5cbb143e
luiznux <luiztagli@hotmail.com>
2021-06-30 23:31:42 -0300

    fix wrong mac port code

--- emacs/.emacs.d/lisp/extra/customizations.el
+++ emacs/.emacs.d/lisp/extra/customizations.el
@@ -48,18 +48,30 @@
           w32-apps-modifier 'hyper)       ; Menu/App key
     (w32-register-hot-key [s-t]))
 
+   ;; Compatible with Emacs Mac port
    (sys/mac-port-p
-    ;; Compatible with Emacs Mac port
-    (setq mac-option-modifier 'meta
-          mac-command-modifier 'super)
-    (bind-keys ([(super a)] . mark-whole-buffer)
-               ([(super c)] . kill-ring-save)
-               ([(super l)] . goto-line)
-               ([(super q)] . save-buffers-kill-emacs)
-               ([(super s)] . save-buffer)
-               ([(super v)] . yank)
-               ([(super w)] . delete-frame)
-               ([(super z)] . undo))
+    ;; Keybonds
+    (global-set-key [(hyper a)] 'mark-whole-buffer)
+    (global-set-key [(hyper v)] 'yank)
+    (global-set-key [(hyper c)] 'kill-ring-save)
+    (global-set-key [(hyper s)] 'save-buffer)
+    (global-set-key [(hyper l)] 'goto-line)
+    (global-set-key [(hyper w)]
+                    (lambda () (interactive) (delete-window)))
+    (global-set-key [(hyper z)] 'undo)
+
+    ;; mac switch meta key
+    (defun mac-switch-meta nil
+      "switch meta between Option and Command"
+      (interactive)
+      (if (eq mac-option-modifier nil)
+          (progn
+	        (setq mac-option-modifier 'meta)
+	        (setq mac-command-modifier 'hyper)
+	        )
+        (progn
+          (setq mac-option-modifier nil)
+          (setq mac-command-modifier 'meta))))
 
     ;; alert style for macos
     (setq alert-default-style 'osx-notifier)
