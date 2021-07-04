1f13afcded447ca50dac3c412f1bcb7d4f62f635
luiznux <luiztagli@hotmail.com>
2021-07-04 01:43:56 -0300

    add impatient mode

--- emacs/.emacs.d/lisp/extra/other-modes.el
+++ emacs/.emacs.d/lisp/extra/other-modes.el
@@ -131,7 +131,9 @@
     :config
     (setq web-mode-markup-indent-offset 2)
     (setq web-mode-css-indent-offset 2)
-    (setq web-mode-code-indent-offset 2)))
+    (setq web-mode-code-indent-offset 2))
+
+  (use-package impatient-mode))
 
 
 (setup-other-modes)
