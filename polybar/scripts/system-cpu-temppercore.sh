d85d3677c0d2470d1dbfdec8d7b5287865752bee
luiznux <luiztagli@hotmail.com>
2021-09-19 00:57:19 -0300

    add amd cpu view

--- polybar/scripts/system-cpu-temppercore.sh
+++ polybar/scripts/system-cpu-temppercore.sh
@@ -6,4 +6,7 @@ temp=$(sensors | grep "CPU" | cut -b 18,19,22,23,24)
 #desktop format
 #temp=$(sensors | grep "Package id 0" | cut -b 17,18,21,22,23)
 
+#Amd format
+#temp=$(sensors | grep "CPUTIN" | cut -b 29,30,33,34,35,36,37)
+
 echo $temp
