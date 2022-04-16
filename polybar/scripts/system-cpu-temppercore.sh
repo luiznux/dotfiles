751d82ad5beda1257f8867ec89c296f8adaee33a
luiznux <luiztagli@hotmail.com>
2022-04-16 14:55:44 -0300

    better output for cpu temp and better doc

--- polybar/scripts/system-cpu-temppercore.sh
+++ polybar/scripts/system-cpu-temppercore.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-#notebook format
-temp=$(sensors | grep "CPU" | cut -b 18,19,22,23,24)
+# Chose the right output dependending on which system are you using
+# Just uncomment one output  and see if it work
 
-#desktop format
-#temp=$(sensors | grep "Package id 0" | cut -b 17,18,21,22,23)
+#notebook Intel format
+#sensors | grep "CPU" | cut -b 18,19,22,23,24 | awk '{print $1}'
 
-#Amd format
-#temp=$(sensors | grep "CPUTIN" | cut -b 29,30,33,34,35,36,37)
+#desktop Intel format
+#sensors | grep "Package id 0" | cut -b 17,18,21,22,23 | awk '{print $1}'
 
-echo $temp
+#Amd CPU format
+#sensors | grep "Tctl" | cut -b 16,17,20,21,22 | awk '{print $1}'
