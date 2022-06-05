1654d1d0bda466d8284911aa5ae781370c532437
luiznux <luiztagli@hotmail.com>
2022-06-05 03:18:27 -0300

    add fzf ohmyzsh plugins to install list

--- install.sh
+++ install.sh
@@ -381,6 +381,7 @@ zsh_setup(){
     && cp $dotfiles/config/.zshrc ~/ \
     && git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions \
     && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting \
+    && git clone https://github.com/joshskidmore/zsh-fzf-history-search ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-fzf-history-search \
     && log echo "     Zsh config {OK} " && break_line || log erro_msg
 }
 
