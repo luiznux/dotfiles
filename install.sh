ccaed3f24948afbb1e01d303db296b65cc4c583d
luiznux <luiztagli@hotmail.com>
2022-07-05 00:41:30 -0300

    add usbutils package

--- install.sh
+++ install.sh
@@ -85,7 +85,7 @@ install_packages(){
 
     linux_gadgets=" man tree colordiff exa fzf wget check file highlight atool mlocate ripgrep nmap ntp ncdu haveged dmidecode hwdetect "
 
-    utilities=" htop atop calcurse cpupower dmenu rofi cmatrix neofetch ranger sl youtube-dl pacmanlogviewer expac xfce4-settings lxinput hddtemp lm_sensors xsensors psensor gedit baobab s-tui smartmontools "
+    utilities=" htop atop calcurse cpupower dmenu rofi cmatrix neofetch ranger sl youtube-dl pacmanlogviewer expac xfce4-settings lxinput hddtemp lm_sensors xsensors psensor gedit baobab s-tui smartmontools usbutils "
 
     program_languages=" global ctags clang ccls go gopls gobject-introspection bash-language-server clisp cargo openjdk11-src shellcheck clojure leiningen nodejs m17n-lib "
 
