2d52776455c5f3b902f3c05bf6893f10d24e06b8
luiznux <luiztagli@hotmail.com>
2022-08-14 12:04:04 -0300

    add bear(compilation database for Clang) package

--- install.sh
+++ install.sh
@@ -86,7 +86,7 @@ install_packages(){
 
     utilities=" htop atop calcurse cpupower dmenu rofi cmatrix neofetch ranger sl youtube-dl pacmanlogviewer expac xfce4-settings lxinput hddtemp lm_sensors xsensors psensor gedit baobab s-tui smartmontools usbutils "
 
-    program_languages=" global ctags clang ccls go gopls gobject-introspection bash-language-server clisp cargo openjdk11-src shellcheck clojure leiningen nodejs m17n-lib "
+    program_languages=" global ctags clang bear ccls go gopls gobject-introspection bash-language-server clisp cargo openjdk11-src shellcheck clojure leiningen nodejs m17n-lib "
 
     graphic=" i3-gaps lxrandr-gtk3 arandr qt zenity dunst picom lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings lightdm-slick-greeter "
 
