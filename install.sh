e6fff26c65ca34f1b1af0807ec4df8e8d7934e62
luiznux <luiztagli@hotmail.com>
2023-02-26 22:59:54 -0300

    [Package] - update catppuccin theme
    Catppuccin changed the package name related with the color palette.
    
    More details at: https://github.com/catppuccin/gtk

--- install.sh
+++ install.sh
@@ -170,7 +170,7 @@ AUR_install(){
     log echo "----------------------------- Installing AUR General packages" && break_line || log erro_msg
     packages=" polybar archlinux-artwork i3lock-color-git autotiling nwg-launchers thermald mictray qdirstat jmtpfs zscroll-git clojure-lsp-bin speedometer cli-visualizer rar mon2cam-git fancontrol-gui "
     fonts=" ttf-weather-icons "
-    themes=" catppuccin-gtk-theme themix-full-git "
+    themes=" catppuccin-gtk-theme-mocha themix-full-git "
     wps_office=" ttf-wps-fonts wps-office-mui-pt-br wps-office "
 
     log_error yay -Syu \
